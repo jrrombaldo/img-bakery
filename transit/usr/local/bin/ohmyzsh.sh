8e21a61962f6ea057a57cf4aa137d2af498cc5b4
jr.rombaldo <jr.rombaldo@gmail.com>
2020-01-12 17:34:51 +0000

    github to build docker

--- transit/usr/local/bin/ohmyzsh.sh
+++ transit/usr/local/bin/ohmyzsh.sh
@@ -15,17 +15,3 @@ cat /etc/pam.d/chsh | grep pam_shells.so;
 chsh -s ~/.oh-my-zsh;
 sed -i  -r -e  "s/^auth\s+sufficient\s+pam_shells.so/auth           required    pam_shells.so/g"  /etc/pam.d/chsh;
 cat /etc/pam.d/chsh | grep pam_shells.so;
-
-
-
-
-#    sudo -H -u pi sh -c 'cd ~/; sh -c  "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended'
-#    sudo -H -u pi sh -c "cd ~/; git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions"
-#    sudo -H -u pi sh -c "echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh'>> ~/.zshrc"
-#    # disabling password prompt for chsh (hacky). The option "--unattended" does not set ZSH as the default shell
-#    sed -i  -r -e  's/^auth\s+required\s+pam_shells.so/auth         sufficient  pam_shells.so/g'  /etc/pam.d/chsh
-#    cat /etc/pam.d/chsh | grep pam_shells.so
-#    chsh -s ~/.oh-my-zsh
-#    # re-enabling password prompt for chsh
-#    sed -i  -r -e  's/^auth\s+sufficient\s+pam_shells.so/auth           required    pam_shells.so/g'  /etc/pam.d/chsh
-#    cat /etc/pam.d/chsh | grep pam_shells.so

9671329789e4062c413c7de57d0f9330dbd84c48
Carlos Rombaldo Jr <jr.rombaldo@gmail.com>
2020-02-12 16:33:09 +0000

    github actions

--- build.sh
+++ build.sh
@@ -1,16 +1,12 @@
 IMG_NAME="jrromb/img-bakery" 
 
-cd ./docker
-docker build . --file Dockerfile --tag $IMG_NAME
-cd ../
-
 IMG_URL=${IMG_URL:="https://downloads.raspberrypi.org/raspbian_lite_latest"}
 
 RESULT="$PWD/result"
 TRANSIT="$PWD/transit"
 SCRIPT="$PWD/setup-image.sh"
 
-docker run -it --rm \
+docker run -id --rm \
  -e IMG_URL="${IMG_URL}" \
  -v ${TRANSIT}:/transit \
  -v ${RESULT}:/result \

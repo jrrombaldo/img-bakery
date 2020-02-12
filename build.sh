aaff1a7c473574edcaf1dd47ae4f05da662940c6
Carlos Rombaldo Jr <jr.rombaldo@gmail.com>
2020-02-12 19:11:45 +0000

    testing build

--- build.sh
+++ build.sh
@@ -1,20 +1,21 @@
-IMG_NAME="jrromb/img-bakery" 
+set -x
+sudo apt install qemu-user-static -y
 
-cd ./docker
-docker build . --file Dockerfile --tag $IMG_NAME
-cd ../
 
+IMG_NAME=${IMG_NAME:="jrromb/img-bakery"}
 IMG_URL=${IMG_URL:="https://downloads.raspberrypi.org/raspbian_lite_latest"}
-
-RESULT="$PWD/result"
-TRANSIT="$PWD/transit"
-SCRIPT="$PWD/setup-image.sh"
+RESULT=${RESULT:="$PWD/result"}
+TRANSIT=${TRANSIT="$PWD/transit"}
+SCRIPT=${SCRIPT:"$PWD/setup-image.sh"}
 
 docker run -i --rm \
- -e IMG_URL="${IMG_URL}" \
- -v ${TRANSIT}:/transit \
- -v ${RESULT}:/result \
- -v ${SCRIPT}:/setup-image.sh \
- -v /dev:/dev \
- --privileged \
- jrromb/img-bakery
+-e IMG_URL="${IMG_URL}" \
+-v ${TRANSIT}:/transit \
+-v ${RESULT}:/result \
+-v ${SCRIPT}:/setup-image.sh \
+-v /dev:/dev \
+--privileged \
+${IMG_NAME}
+
+
+#  result=./result/rasp-custom.img.gz
